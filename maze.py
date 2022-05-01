492ae28db64a6dc4090bfe3a243ac507337462cc
Zach Rickers <mzrickers@gmail.com>
2022-05-01 10:15:18 +0200

    printing maze

--- maze.py
+++ maze.py
@@ -18,12 +18,23 @@ maze = [
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
 
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            stdscr.addstr(i, j, value)
+    
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     blue_and_black = curses.color_pair(1)
+    
 
     stdscr.clear()
-    stdscr.addstr(0, 0,"hello world", blue_and_black) 
+    print_maze(maze, stdscr) 
     stdscr.refresh()
     stdscr.getch()
 
